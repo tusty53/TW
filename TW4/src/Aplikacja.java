696425f84636348cb2ae84fe5b4439716f26c942
Kuba <Kuba@Kuba-PC>
2016-04-02 14:38:06 +0200

    Base files

---
+++ TW4/src/Aplikacja.java
@@ -0,0 +1,10 @@
+
+
+
+class Aplikacja {
+	public static void main(String args[]){
+		
+		Executor_2 e = new Executor_2();
+		e.start();
+	}
+}
---
+++ TW4/src/Vertex.java
@@ -0,0 +1,31 @@
+
+
+
+class Vertex {
+	//constructor
+	Vertex(Vertex Left, Vertex Right, String Lab){
+		this.m_left=Left;
+		this.m_right=Right;
+		this.m_label=Lab;
+	}
+	//empty constructor
+	Vertex(){
+		this.m_left=null;
+		this.m_right=null;
+	}
+	//label
+	String m_label;
+	//links to adjacent elements
+	Vertex m_left;
+	Vertex m_right;
+	//methods for adding links
+	void set_left(Vertex Left){
+		m_left=Left;
+	}
+	void set_right(Vertex Right){
+		m_right=Right;
+	}
+	void set_label(String Lab){
+		m_label=Lab;
+	}
+}
